db9dbf07c73d94f61808144dd089665be4130fcf
DeoluBe <91130207+DeoluBe@users.noreply.github.com>
2026-01-15 01:27:18 +0100

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -21,7 +21,7 @@ export default function CTA() {
       </motion.div>
 
       <motion.img
-        src="/"
+        src="/default copy.svg"
         alt=""
         className="mx-auto h-24 w-24"
         variants={itemVariants}
