a24363d4387b3f5f423409069c4ff9e4ae90c235
DeoluBe <91130207+DeoluBe@users.noreply.github.com>
2026-01-16 11:20:54 +0100

    Update alt text for Rebox logo

--- components/logos.tsx
+++ components/logos.tsx
@@ -5,7 +5,7 @@ import { containerVariants, itemVariants } from "@/lib/animation-variants";
 import TextBlur from "./ui/text-blur";
 
 const logos = [
-  { href: "https://reboxfinance.com", src: "/default copy.svg", alt: "Rebox Logo" },
+  { href: "https://reboxfinance.com", src: "/default copy.svg", alt: "ReboxLogo" },
 ];
 
 export default function Logos() {
