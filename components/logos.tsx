46fe7a23bbdf2caa56a3d7fc15756f5026928117
DeoluBe <91130207+DeoluBe@users.noreply.github.com>
2026-01-16 10:51:34 +0100

    Replace Notion logo with Rebox logo

--- components/logos.tsx
+++ components/logos.tsx
@@ -5,8 +5,7 @@ import { containerVariants, itemVariants } from "@/lib/animation-variants";
 import TextBlur from "./ui/text-blur";
 
 const logos = [
-  { href: "https://nextjs.org", src: "/nextjs.svg", alt: "Next.js Logo" },
-  { href: "https://notion.so", src: "/default copy.svg", alt: "Notion Logo" },
+  { href: "https://reboxfinance.com", src: "/default copy.svg", alt: "Rebox Logo" },
 ];
 
 export default function Logos() {
@@ -33,23 +32,20 @@ export default function Logos() {
 
       <motion.div
         variants={itemVariants}
-        className="mt-4 grid w-full grid-cols-2 items-center justify-center gap-4 md:mt-6 md:grid-cols-3 md:gap-6">
-        {logos.map((logo, index) => (
-          <Link
-            key={index}
-            href={logo.href}
-            rel="noopener noreferrer"
-            target="_blank"
-            className="flex h-24 items-center justify-center rounded-lg border bg-zinc-900 p-8 transition-all duration-150 ease-in-out md:hover:border-zinc-700 md:hover:bg-accent">
-            <Image
-              src={logo.src}
-              alt={logo.alt}
-              width={100}
-              height={100}
-              className="h-auto w-32 opacity-85"
-            />
-          </Link>
-        ))}
+        className="mt-4 flex w-full items-center justify-center md:mt-6">
+        <Link
+          href={logos[0].href}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="flex h-24 items-center justify-center rounded-lg border bg-zinc-900 p-8 transition-all duration-150 ease-in-out md:hover:border-zinc-700 md:hover:bg-accent">
+          <Image
+            src={logos[0].src}
+            alt={logos[0].alt}
+            width={100}
+            height={100}
+            className="h-auto w-32 opacity-85"
+          />
+        </Link>
       </motion.div>
     </motion.div>
   );
